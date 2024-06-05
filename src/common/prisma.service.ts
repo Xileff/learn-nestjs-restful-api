b3e7377a28bd9d2bd31e60b27db2def27df67721
Felix <87980522+Xileff@users.noreply.github.com>
2024-06-05 15:19:52 +0700

    feat: setup PrismaService

---
+++ src/common/prisma.service.ts
@@ -0,0 +1,52 @@
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
+@Injectable() // Supaya bisa DI ke module lain
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, string>
+  implements OnModuleInit
+{
+  // Extend dari PrismaClient untuk tambahin custom logger
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    // Manggil super constructor untuk bikin semua level aktivitas ditembak sebagai 'event'
+    super({
+      log: [
+        {
+          level: 'info',
+          emit: 'event',
+        },
+        {
+          level: 'warn',
+          emit: 'event',
+        },
+        {
+          level: 'error',
+          emit: 'event',
+        },
+        {
+          level: 'query',
+          emit: 'event',
+        },
+      ],
+    });
+  }
+
+  // Buat event listeners untuk logger ketika terjadi 'event'
+  onModuleInit() {
+    this.$on('info', (e) => {
+      this.logger.log(e);
+    });
+    this.$on('warn', (e) => {
+      this.logger.log(e);
+    });
+    this.$on('error', (e) => {
+      this.logger.log(e);
+    });
+    this.$on('query', (e) => {
+      this.logger.log(e);
+    });
+  }
+}
